41e96511e1f10f15cc15e7feef256cb48c76fcce
outtaspace <flyweight@yandex.ru>
2017-03-04 13:24:09 +0200

    Переписана работа с CLI. Переписан код транслирующий параметры из CLI в параметры команд.

--- run.py
+++ run.py
@@ -2,33 +2,49 @@ import argparse
 import os
 import sys
 
+from city_info.command_factory import CommandFactory
+
 
 def get_cli_options():
     parser = argparse.ArgumentParser()
-
-    parser.add_argument('--all', action='store_true', help='provide summary information for all cities')
-    parser.add_argument('--filename', type=str, help='optional filename holding the city info')
-    parser.add_argument('--city', type=str, help='name of city')
+    parser.add_argument(
+        '--all',
+        action='store_true',
+        help='provide summary information for all cities'
+    )
+    parser.add_argument(
+        '--filename',
+        type=str,
+        help='optional filename holding the city info'
+    )
+    parser.add_argument(
+        '--city',
+        type=str,
+        help='name of city'
+    )
 
     return parser.parse_args()
 
 
 def main():
-    options = get_cli_options()
-    print(options)
+    cli_options = get_cli_options()
 
-    filename = options.filename
-    if filename is None:
-        filename = os.path.abspath(__file__)
+    options = dict(writer=sys.stdout)
 
-    xargs = dict(
-        reader=open(filename, 'r'),
-        writer=sys.stdout,
-        city=options.city
-    )
+    if cli_options.all:
+        options['mode'] = 'all'
+    else:
+        options['mode'] = 'city'
+        options['city'] = cli_options.city
 
-    command = command_factory.create('city' if options.city else 'all', xargs)
+    if cli_options.filename is None:
+        filename = os.path.abspath('./city_info.txt')
+    else:
+        filename = cli_options.filename
+    options['reader'] = open(filename, 'r')
+
+    command = CommandFactory.create(**options)
     command.execute()
 
-if __name__ == '__main__':
-    main()
+
+main()
