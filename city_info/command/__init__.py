6c76456a58c94431e27f65c723c7af7e2717bca1
outtaspace <flyweight@yandex.ru>
2017-03-19 19:52:56 +0200

    Удалены сеттеры для свойств reader, writer - не использовались.

--- city_info/command/__init__.py
+++ city_info/command/__init__.py
@@ -7,19 +7,9 @@ class ICommandIOLayer(abc.ABC):
     def reader(self) -> object:
         pass
 
-    @reader.setter
-    @abc.abstractmethod
-    def reader(self, reader):
-        pass
-
     @property
     @abc.abstractmethod
     def writer(self):
-        pass
-
-    @writer.setter
-    @abc.abstractmethod
-    def writer(self, writer):
         pass
 
 
@@ -36,14 +26,6 @@ class Command(ICommandIOLayer, abc.ABC):
     def reader(self) -> object:
         return self.__reader
 
-    @reader.setter
-    def reader(self, reader):
-        self.__reader = reader
-
     @property
     def writer(self):
         return self.__writer
-
-    @writer.setter
-    def writer(self, writer):
-        self.__writer = writer
